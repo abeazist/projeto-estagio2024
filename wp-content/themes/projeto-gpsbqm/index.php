c72ccae29a109aff7dc7b589237cd4707f0e35bb
beatriz <128046453+abeazist@users.noreply.github.com>
2024-07-09 18:11:31 -0300

    começando a dar forma para o html

---
+++ wp-content/themes/projeto-gpsbqm/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grupo de Pesquisa UTFPR-CM</title>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <section class="menu-area">
+            <section class="logo"></section>
+            <nav class="menu">
+
+            </nav>
+        </section>
+    </header>
+    <div class="content-area">
+        <main>
+            <section class="slide"></section>
+            <section class="services"></section>
+            <section class="middle-area"></section>
+        </main>
+    </div>
+
+</body>
+</html>
