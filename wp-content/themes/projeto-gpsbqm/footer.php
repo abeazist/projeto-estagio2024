b659cf30d3dbe963bb1ab3bb157ba5972ebcabbd
beatriz <128046453+abeazist@users.noreply.github.com>
2024-07-11 18:20:41 -0300

    nav ó, tá lindo

---
+++ wp-content/themes/projeto-gpsbqm/footer.php
@@ -0,0 +1,8 @@
+<footer>
+        <div class="rodape">
+            <a href="https://github.com/abeazist" target="_blank" >GitHub<img src="" alt=""></a>
+        </div>
+    </footer>
+
+</body>
+</html>
--- wp-content/themes/projeto-gpsbqm/index.php
+++ wp-content/themes/projeto-gpsbqm/index.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="PT-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Grupo de Pesquisa UTFPR-CM</title>
-</head>
-<body <?php body_class(); ?>>
-    <header>
-        <section class="menu-area">
-            <section class="logo"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="40px" fill="#000000"><path d="M181.33-120q-44.85 0-63.76-41.17-18.9-41.16 11.1-75.5L368-513.33v-260h-48q-14.17 0-23.75-9.62t-9.58-23.83q0-14.22 9.58-23.72 9.58-9.5 23.75-9.5h320q14.17 0 23.75 9.62 9.58 9.61 9.58 23.83 0 14.22-9.58 23.72-9.58 9.5-23.75 9.5h-48v260l239.33 276.66q30 34.34 11.1 75.5Q823.52-120 778.67-120H181.33Zm-8-66.67h613.34L525.33-489.33v-284h-90.66v284L173.33-186.67ZM478.67-480Z"/></svg>GPSBQM</section>
-            <nav class="menu">
-                <ul>
-                    <li><a href="">Início</a></li>
-                    <li><a href="">Projetos</a></li>
-                    <li><a href="">Equipe</a></li>
-                    <li><a href="">Fotos</a></li>
-                    <li><a href=""><button>Conta</button></a></li>
-                </ul>
-
-            </nav>
-        </section>
-    </header>
+<?php get_header(); ?>
         <main>
             <div class="area-container">
             <div class="inicio">
@@ -44,11 +21,4 @@ Campo Mourão, acompanhe em seus projetos em seus estudos.</p>
 
             </div>
         </main>
-    <footer>
-        <div class="rodape">
-            <a href="https://github.com/abeazist" target="_blank" >GitHub<img src="" alt=""></a>
-        </div>
-    </footer>
-
-</body>
-</html>
+<?php get_footer(); ?>
